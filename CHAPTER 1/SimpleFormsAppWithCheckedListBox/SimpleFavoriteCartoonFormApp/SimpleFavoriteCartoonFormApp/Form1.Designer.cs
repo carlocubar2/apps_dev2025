7987d642f1917968217121c2e4153ac207d07535
carlocubar2 <carlocubar.2@gmail.com>
2025-03-24 19:31:35 +0800

    Add files via upload

---
+++ CHAPTER 1/SimpleFormsAppWithCheckedListBox/SimpleFavoriteCartoonFormApp/SimpleFavoriteCartoonFormApp/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace SimpleFavoriteCartoonFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.BtnSelect_Click = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(538, 181);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(340, 250);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(210, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SimpleFavoriteCartoonFormApp";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(342, 217);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // BtnSelect_Click
+            // 
+            this.BtnSelect_Click.Location = new System.Drawing.Point(333, 270);
+            this.BtnSelect_Click.Name = "BtnSelect_Click";
+            this.BtnSelect_Click.Size = new System.Drawing.Size(75, 23);
+            this.BtnSelect_Click.TabIndex = 3;
+            this.BtnSelect_Click.Text = "select";
+            this.BtnSelect_Click.UseVisualStyleBackColor = true;
+            this.BtnSelect_Click.Click += new System.EventHandler(this.BtnSelect_Click_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(333, 345);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1218, 574);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.BtnSelect_Click);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button BtnSelect_Click;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ CHAPTER 1/SimpleFormsAppWithCheckedListBox/SimpleFavoriteCartoonFormApp/SimpleFavoriteCartoonFormApp/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SimpleFavoriteCartoonFormApp
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Visible = true;
+            comboBox1.Items.Add("Spongebob");
+            comboBox1.Items.Add("Patrick");
+            comboBox1.Items.Add("Mr. Krabs");
+            comboBox1.Items.Add("Sandy");
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSelect_Click_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a character!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string selectedCharacter = comboBox1.SelectedItem.ToString();
+            string imagePath = "";
+
+            if (selectedCharacter == "Spongebob")
+                imagePath = "spongebob.jpg";
+            else if (selectedCharacter == "Patrick")
+                imagePath = "patrick.jpg";
+            else if (selectedCharacter == "Mr. Krabs")
+                imagePath = "mrkrabs.jpg";
+            else if (selectedCharacter == "Sandy")
+                imagePath = "sandy.jpg";
+
+            if (File.Exists(imagePath))
+            {
+                pictureBox1.Image = Image.FromFile(imagePath);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                MessageBox.Show("Image not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnClear_Click_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            comboBox1.SelectedIndex = -1;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
